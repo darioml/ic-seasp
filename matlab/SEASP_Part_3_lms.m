71cbfecd7fe8ab594f26ac5df833e4852284a0c5
= <dario_dml@hotmail.com>
2015-03-31 00:15:27 +0100

    Started work on b

--- matlab/SEASP_Part_3_lms.m
+++ matlab/SEASP_Part_3_lms.m
@@ -15,12 +15,3 @@ function [ y,e,a ] = SEASP_Part_3_lms( x, mu, p )
         a(:, m+1) = a(:,m) + mu*e(m)*x( m-1:-1:m-p );
     end
 end
-
-
-function [mu] = func_mu ( error )
-    % high error, high mu
-    % low error, low mu
-    % error under 1 is good, above 1 is bad. 10 is worst
-    mu = min( max(0.01, 0.055*error - 0.045),  0.5  ) ;
-end
-
