7c3b00d7f011c735ccda98086fa8c40f344b921c
= <dario_dml@hotmail.com>
2015-04-07 06:24:40 +0100

    tried to do 1.3. Cant do it

--- matlab/SEASP_Part_1_3_a.m
+++ matlab/SEASP_Part_1_3_a.m
@@ -1,2 +1,31 @@
 clear all
 close all
+
+%%
+fftsize = 10000;
+N = 20;
+x = bartlett(N);
+fftshifted = abs(fftshift(fft(x, fftsize)));
+stem(-fftsize/2:(fftsize/2)-1,fftshifted);
+
+max = fftshifted(1+fftsize/2);
+length(fftshifted(fftshifted> (max/2) ))
+
+threedb = zeros(100,1);
+sidelobe = zeros(100,1);
+for n=3:100
+    x = bartlett(n);
+    fftshifted = abs(fftshift(fft(x, fftsize)));
+
+    max = fftshifted(1+fftsize/2);
+    threedb(n) = length(fftshifted(fftshifted> (max/2) ));
+    
+    x = findpeaks(fftshifted);
+    if (length(x) > 3)
+        sidelobe(n)= 10*log10(x((length(x)-1)/2));
+    end
+end
+
+plot(3:100,threedb(3:end))
+figure
+plot(sidelobe)
--- matlab/SEASP_Part_1_3_b.m
+++ matlab/SEASP_Part_1_3_b.m
@@ -1,2 +1,8 @@
 clear all
 close all
+
+f_0 = 0.2;
+x_n = @(a_1, a_2, phi_1, phi_2, sigma2, alpha, N) a_1*sin(f_0*2*pi*(0:(N-1)) + phi_1) + ...
+    a_2*sin((f_0+alpha/N)*2*pi*(0:(N-1)) + phi_2) + sqrt(sigma2)*randn([1 N]);
+
+plot(x_n(1, 0, 0, 0, 0, 1, 1000))
